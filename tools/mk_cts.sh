4d933f6b0b03ae4562b260e10525c0f6685e54f8
Alwin-Lin <alwin001@gmail.com>
2020-09-06 22:36:39 +0000

    Add mk_cts.sh to build CTS and libaries

---
+++ tools/mk_cts.sh
@@ -0,0 +1,11 @@
+SECOND=0
+mkdir -p $HOME/ws/libs
+cd $HOME/ws/android
+source build/envsetup.sh
+lunch aosp_x86_64-userdebug
+make cts cts-sensors-tests compatibility-device-util-axt android.test.base_static -j8
+cp out/target/product/generic_x86_64/obj/JAVA_LIBRARIES/cts-sensors-tests_intermediates/javalib.jar $HOME/ws/libs/sensors-tests.jar
+cp out/target/product/generic_x86_64/obj/JAVA_LIBRARIES/compatibility-device-util-axt_intermediates/javalib.jar $HOME/ws/libs/compatibility-device-util-axt.jar
+cp out/target/product/generic_x86_64/obj/JAVA_LIBRARIES/android.test.base_static_intermediates/javalib.jar $HOME/ws/libs/android.test.base.jar
+echo "Build time $SECOND seconds"
+
