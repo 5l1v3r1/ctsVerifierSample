102de97c403ed8390e3b70633fce6bf3d28d2d1a
Alwin-Lin <alwin001@gmail.com>
2020-09-07 17:22:27 +0000

    Fix bash script second counting

--- tools/mk_cts.sh
+++ tools/mk_cts.sh
@@ -1,4 +1,4 @@
-SECOND=0
+SECONDS=0
 mkdir -p $HOME/ws/libs
 cd $HOME/ws/android
 source build/envsetup.sh
@@ -7,5 +7,5 @@ make cts cts-sensors-tests compatibility-device-util-axt android.test.base_stati
 cp out/target/product/generic_x86_64/obj/JAVA_LIBRARIES/cts-sensors-tests_intermediates/javalib.jar $HOME/ws/libs/sensors-tests.jar
 cp out/target/product/generic_x86_64/obj/JAVA_LIBRARIES/compatibility-device-util-axt_intermediates/javalib.jar $HOME/ws/libs/compatibility-device-util-axt.jar
 cp out/target/product/generic_x86_64/obj/JAVA_LIBRARIES/android.test.base_static_intermediates/javalib.jar $HOME/ws/libs/android.test.base.jar
-echo "Build time $SECOND seconds"
+echo "Build time $SECONDS seconds"
 
